815d1a2f57bd044933bd99049a7a7ed9661d74cb
Shantito10 <chantelmantentela@gmail.com>
2023-07-18 12:32:44 -0700

    what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - extract last number of a variable
+ * @n: variable where we extract last number
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 49);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 49);
+		return (1);
+	}
+}
