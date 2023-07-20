33e95a345cfb26a89736e52100c2e68f31591e62
Shantito10 <chantelmantentela@gmail.com>
2023-07-20 06:31:54 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_triangle - print backwards triangle of n size
+ * @size: size of square
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
