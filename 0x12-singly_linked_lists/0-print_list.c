10bc938084e25ce34d64351e79b522d9e64578c0
Shantito10 <chantelmantentels@gmail.com>
2023-08-24 14:33:49 -0700

    woah yoh yoh yoh

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,19 +7,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n_nodes;
+	unsigned int size;
 
-	if (!h)
-		return (0);
-	n_nodes = 0;
-	do {
-		if (!h->str)
+	size = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
-		n_nodes++;
-	} while (h);
+		size += 1;
+	}
 
-	return (n_nodes);
+	return (size);
 }
