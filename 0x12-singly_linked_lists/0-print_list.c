295560a6bc1fafb7753873826fd395feaa9e3202
Shantito10 <chantelmantentels@gmail.com>
2023-08-24 13:59:55 -0700

    not working out

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t n_nodes;
 
-	nelem = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+	if (!h)
+		return (0);
+	n_nodes = 0;
+	do {
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
-	}
-	return (nelem);
+		n_nodes++;
+	} while (h);
+
+	return (n_nodes);
 }
