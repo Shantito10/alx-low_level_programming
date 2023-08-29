ccd1732cf4f44afa898415010e31696f2f29f4c4
Shantito10 <chantelmantentels@gmail.com>
2023-08-29 12:13:15 -0700

    frees list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,16 +3,20 @@
 /**
  * free_listint2 - frees a listint_t list
  * @head: head of a list
- * Return: 0 or NULL if fail
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *count;
+	listint_t *current;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		count = *head;
-		*head = (*head)->next;
-		free(count);
+		current = *head;
+		while ((count = current) != NULL)
+		{
+			current = current->next;
+			free(count);
+		}
+		*head = NULL;
 	}
 }
